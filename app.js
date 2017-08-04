5bc1d01024a555a948bc0baf7a12ee997108d660
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-08-03 23:51:16 -0400

    added blank endpoints

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/tododb');
+
+const Todo = require('./models/todo');
+
 const app = express();
 
 app.use('/static', express.static('static'));
@@ -10,13 +16,32 @@ app.get('/', function (req, res) {
 });
 
 // put routes here
-app.get('api/todos', (req, res) =>{
-  res.json(todos);
+app.get('/api/todos', (req, res) =>{
+  Todo.find({}).then((todos) => {
+    res.render('todo', {model: todos});
+  });
 });
 
-app.post('/api/todos', function(req, res){
-  
+app.post('/api/todos', (req, res) => {
+  console.log(req.body);
 });
+// GET /api/todos/:id  get a specific todo item
+app.get('/api/todos/:id', (req, res) => {
+
+});
+// PUT /api/todos/:id  update a todo item. Returns modified todo item
+app.put('/api/todos/:id', (req, res) => {
+
+});
+// PATCH /api/todos/:id  partially update a todo item. Returns the modified todo item
+app.patch('/api/todos/:id', (req, res) => {
+
+});
+// DELETE /api/todos/:id  deletes a todo item. Returns the todo item that was deleted
+app.delete('/api/todos/:id', (req, res) =>{
+
+});
+
 
 // app.post('/api/todos').then(response =>{
 //   console.log('response'. response);
